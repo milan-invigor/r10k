051cc8c036db303ebb9560ef3f051bb22d662ea1
milan.mitic <milan.mitic@invigorgroup.com>
2018-09-13 10:59:22 +1000

    adding puppetdb/hiera dependencies

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,4 +1,13 @@
 class profile::base {
   notify{ 'test 2': }
   class { '::ntp': }
+
+  # PuppetDB
+  include ::puppetdb
+  include ::puppetdb::master::config
+
+  # Hiera
+  package { ['hiera', 'hiera-puppet']:
+    ensure => present,
+  }
 }
